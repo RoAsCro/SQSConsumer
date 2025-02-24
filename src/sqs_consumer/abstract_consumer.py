adbd47db8df928bb0ed417bb11e98d2614030d4d
Roland <rollycrompton@gmail.com>
2025-02-24 19:36:59 +0000

    Classified

--- src/sqs_consumer/abstract_consumer.py
+++ src/sqs_consumer/abstract_consumer.py
@@ -1,83 +1,87 @@
 import logging
-import os
 import threading
+from abc import ABC
 
 import boto3
-from dotenv import load_dotenv
-from flask import Blueprint
+from flask import Blueprint, Flask
 
-default_region = "us-east-1"
+class AbstractConsumer(ABC):
 
-load_dotenv()
-# Environment variables
-queue = os.getenv("QUEUE")
-aws_region = os.getenv("AWS_REGION")
-if aws_region is None:
-    aws_region = default_region
-access_id = os.getenv("AWS_ACCESS_KEY_ID")
-access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    default_region = "us-east-1"
+    env = {}
+    # Environment variables
+    queue = env.get("QUEUE")
+    aws_region = env.get("AWS_REGION")
+    if aws_region is None:
+        aws_region = default_region
+    access_id = env.get("AWS_ACCESS_KEY_ID")
+    access_key = env.get("AWS_SECRET_ACCESS_KEY")
 
-exception = Exception
-router = Blueprint("messages", __name__, url_prefix="/queue_1")
+    exception = Exception
+    router = Blueprint("messages", __name__, url_prefix="/queue_1")
+    def __init__(self, env):
+        self.env = env
 
+    sqs = boto3.client("sqs",
+                       region_name=aws_region,
+                       aws_access_key_id=access_id,
+                       aws_secret_access_key=access_key
+                       )
 
-sqs = boto3.client("sqs",
-                   region_name=aws_region,
-                   aws_access_key_id=access_id,
-                   aws_secret_access_key=access_key
-                   )
-
-running = False
-
-
-def get_from_queue():
-
-    response = sqs.receive_message(
-        QueueUrl=queue,
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=["All"],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=20
-    )
-
-    if "Messages" not in response:
-        return None
-
-    message = response["Messages"][0]
-
-    return message
-
-def send(message_to_send):
-    pass
-
-def delete(message):
-    receipt_handle = message["ReceiptHandle"]
-
-    sqs.delete_message(
-        QueueUrl=queue,
-        ReceiptHandle=receipt_handle
-    )
-
-def process():
-    global running
-    running = True
-    while running:
-        message = get_from_queue()
-        if message:
-            try:
-                send(message)
-            except exception as ex:
-                logging.error(ex)
-                continue
-            delete(message)
-
-def background_thread():
-    thread = threading.Thread(target=process, daemon=True)
-    thread.start()
-    return thread
+    running = False
 
 
+    def get_from_queue(self):
+        response = self.sqs.receive_message(
+            QueueUrl=self.queue,
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=["All"],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=20
+        )
 
-@router.get("/health")
-def health_check():
-    return 'Ok', 200
+        if "Messages" not in response:
+            return None
+
+        message = response["Messages"][0]
+
+        return message
+
+    def send(self, message_to_send):
+        pass
+
+    def delete(self, message):
+        receipt_handle = message["ReceiptHandle"]
+
+        self.sqs.delete_message(
+            QueueUrl=self.queue,
+            ReceiptHandle=receipt_handle
+        )
+
+    def process(self):
+        self.running = True
+        while self.running:
+            message = self.get_from_queue()
+            if message:
+                try:
+                    self.send(message)
+                except self.exception as ex:
+                    logging.error(ex)
+                    continue
+                self.delete(message)
+
+    def background_thread(self):
+        thread = threading.Thread(target=self.process, daemon=True)
+        thread.start()
+        return thread
+
+
+
+    @router.get("/health")
+    def health_check(self):
+        return 'Ok', 200
+
+    def run(self):
+        health_checker = Flask(__name__)
+        health_checker.register_blueprint(self.router)
+        return health_checker
